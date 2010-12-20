c91a276d1880371f0efc40f0c4da9afb7f43ec8f
jsmith <jsmith@f827c759-226e-4a48-a6ac-82325153066a>
2010-12-20 21:50:19 +0000

    Initial parsing
    git-svn-id: https://svn.devjason.com/projects/spring2graphml@985 f827c759-226e-4a48-a6ac-82325153066a

--- spring2graphml.groovy
+++ spring2graphml.groovy
@@ -1,12 +1,23 @@
+// http://groovy.codehaus.org/groovy-jdk/
+
+def parseAppCtx(file) {
+	def slurper = new XmlSlurper()
+	def beans = slurper.parse(file)
+	beans.bean.each { bean ->
+		println bean.@id.text()
+		bean.attributes().keySet().findAll { it =~ /.*ref/ }
+	}
+}
 
 
-
-
+@Grab(group='org.codehaus.gpars', module='gpars', version='0.11')
 def main() {
 	basedir = '/home/jsmith/jsi/fce-release-2/fcw/WebRoot/WEB-INF'
 	APPCTX_PATTERN = ~/applicationContext.*xml/
+	APPCTX_PATTERN = ~/applicationContext-database.xml/
+	parsers = []
 	new File(basedir).eachFileMatch(APPCTX_PATTERN) { file ->
-		println "Found file $file"
+		parseAppCtx(file)
 	}
 }
 
